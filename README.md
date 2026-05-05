22d8aa1fba84c3ddfd45112d2a26f4fe0146c820
phdinhh <phdinh@ucsd.edu>
2026-05-04 20:24:47 -0700

    explore.js completed and README update

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Lab 5 - Starter
 Name: Phuong Dinh
 
-Site: [expose.html](expose.html)
+Site:  
+[expose.html](expose.html)
+[explore.html](explore.html)
 
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -5,17 +5,45 @@ window.addEventListener('DOMContentLoaded', init);
 function init() {
   // TODO
 
-  let speak = document.querySelector('button');
-  const speakingText = document.querySelector('textarea');
-  const
-  speak.addEventListener('click', function() {  
-    if (horn.value == 'party-horn') {
-      sound.play();
-      jsConfetti.addConfetti();
-    } else {
-      sound.play();
+  
+  // Get voices
+  let voices = [];
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+    // const select = document.getElementById('voice-select');
+    // select.innerHTML = '<option disabled selected>Select Voice:</option>';
+    for (const voice of voices) {
+      const option = document.createElement("option");
+      option.textContent = `${voice.name}(${voice.lang})`;
+      option.setAttribute('data-lang', voice.lang);
+      option.setAttribute('data-name', voice.name);
+      document.getElementById('voice-select').appendChild(option);
     }
+  }
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // Play audio
+  const speak = document.querySelector('button');
+  const speakingText = document.querySelector('textarea');
+  const speakingVoice = document.querySelector('select');
+  const image = document.querySelector('img');
+  speak.addEventListener('click', function() {  
+    let utter = new SpeechSynthesisUtterance(speakingText.value);
+    for (const voice of voices) {
+      if (voice.name === speakingVoice.selectedOptions[0].getAttribute('data-name')) {
+        utter.voice = voice;
+      }
+    }
+    utter.onstart = function() {
+      image.src = 'assets/images/smiling-open.png';
+    }
+    utter.onend = function() {
+      image.src = 'assets/images/smiling.png';
+    }
+    speechSynthesis.speak(utter);
   });
   
-
 }
